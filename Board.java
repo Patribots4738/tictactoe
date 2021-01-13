2263d8f6c5cef139637f49830ceb954648598487
Nicholas264 <22565232+Nicholas264@users.noreply.github.com>
2021-01-12 16:36:19 -0800

    Rewrite entire game
    Ties aren't accounted for yet and finishing a game in a tie will result in no one being able to make a valid move, in which case you should just run the code again to get a new game

--- Board.java
+++ Board.java
@@ -1,70 +1,81 @@
 public class Board {
 
-    // index 0 is top row, 1 is middle row, 2 is bottom row
-    char[][] board = { {'_', '_', '_'},
-                       {'_', '_', '_'}, 
-                       {'_', '_', '_'} };
+	// index 0 is top row, 1 is middle row, 2 is bottom row
+	String[][] board;
+	boolean turn;
 
-    public Board() {}
+	public Board() {
+		this.board = new String[3][3];
+		// initalizes the board with 9 spaces
+		for (int i = 0; i < this.board.length; i++) {
+			for (int j = 0; j < this.board[i].length; j++) {
+				this.board[i][j] = " ";
+			}
+		}
+		this.turn = false;  
+	}
 
-    public void setSpace(char character, int x, int y) {
+	public void makeMove(int x, int y) {
+		// checks to make sure the move is in range
+		if (x <= 2 || x >= 0 || y <= 2 || y >= 0) {
+			// checks to make sure the space isn't already occupied
+			if (board[x][y] != " ") {
+				System.out.println("Invalid move; try again!");
+				return;
+			}
+		} else {
+			System.out.println("Invalid move; try again!");
+			return;
+		}
 
-        board[x][y] = character;
+		board[x][y] = this.whoseTurn();
+		// switches the turn to the opposite player
+		this.turn = !this.turn;
+	}
 
-    }
+	public String getSpace(int x, int y) {
+		return board[x][y];
+	}
 
-    public char getSpace(int x, int y) {
-
-        return board[x][y];
-
-    }
-
-    public char[] getRow(int depth) {
-
-        return board[depth];
-
-    }
-
-    public char[] getColumn(int width) {
-
-        char[] column = { board[0][width], 
-                          board[1][width], 
-                          board[2][width] };
-
-        return column;
-
-    }
-
-    public char[] getDiagonal(char leftRight) {
-
-        char[] diagonal = new char[3];
-
-        if(leftRight == 'L') {
-
-            diagonal[0] = board[0][0];
-            diagonal[1] = board[1][1];
-            diagonal[2] = board[2][2];
-
-        }
-
-        if(leftRight == 'R') {
-
-            diagonal[0] = board[0][1];
-            diagonal[1] = board[1][1];
-            diagonal[2] = board[2][0];
-
-        }
-
-        return diagonal;
-
-    }
-
-    public void printBoard() {
-
-        System.out.println(board[0]);
-        System.out.println(board[1]);
-        System.out.println(board[2]);
-
-    }
-
+	public String whoseTurn() {
+		return this.turn ? "O" : "X";
+	}
+	// returns null if there is no winner, and X or O if there is a winner
+	public String getWinner() {
+		String[][] board = this.board;
+		for (int i = 0; i < board.length; i++) {
+			String[] row = board[i];
+			// checks if row i is 3 of the same character in a row that is not a space
+			if (checkEquals(row[0], row[1], row[2])) {
+				return row[0];
+			}
+			// checks if column i is 3 of the same character in a row that is not a space
+			if (checkEquals(board[0][i], board[1][i], board[2][i])) {
+				return board[0][i];
+			}
+		}
+		// checks diagonals for equality
+		if (checkEquals(board[0][0], board[1][1], board[2][2])) {
+			return board[0][0];
+		}
+		// other diagonal check
+		if (checkEquals(board[0][2], board[1][1], board[2][0])) {
+			return board[0][2];
+		}
+		return null;
+	}
+	// checks if 3 strings are equal and not a space
+	private boolean checkEquals(String s1, String s2, String s3) {
+		return s1 == s2 && s2 == s3 && s1 != " ";
+	}
+	public String toString() {
+		String board = "";
+		for (int i = 0; i < this.board.length; i++) {
+			for (int j = 0; j < this.board[i].length; j++) {
+				board += this.board[i][j] + (j == 2 ? "" : " | ");
+			}
+			board += (i == 2) ? "" : "\n----------\n";
+		}
+		return board;
+	}
 }
--- Main.java
+++ Main.java
@@ -2,60 +2,35 @@ import java.util.Scanner;
 
 public class Main {
 
-    
     public static void main(String... args) {
     
-        for(int i = 0; i < 100; i++) {
+        Board board = new Board();
+        Scanner input = new Scanner(System.in);
 
-            System.out.println();
-
+        // while there is no winner, loop forever
+        while (board.getWinner() == null) {
+            System.out.println("\n" + board.toString() + "\n");
+            System.out.println("Please make your move " + board.whoseTurn() + " (row, col)\n");
+            int[] move = stringToInt(input.nextLine());
+            if (move == null) {
+                System.out.println("Invalid move; try again!");
+                continue;
+            }
+            board.makeMove(move[0], move[1]);
         }
-
-        Board newBoard = new Board();
-
-        newBoard.setSpace('X', 0, 0);
-        newBoard.setSpace('X', 0, 1);
-        newBoard.setSpace('X', 0, 2);
-
-        newBoard.printBoard();
-
-        char[] row1 = newBoard.getRow(0);
-        char[] row2 = newBoard.getRow(1);
-        char[] row3 = newBoard.getRow(2);
-
-        boolean firstRowVictory = (row1[0] == row1[1]) && (row1[1] == row1[2]) && (row1[0] == 'X' || row1[0] == 'O');
-        boolean secondRowVictory = (row2[0] == row2[1]) && (row2[1] == row2[2]) && (row2[0] == 'X' || row2[0] == 'O');
-        boolean thirdRowVictory = (row3[0] == row3[1]) && (row3[1] == row3[2]) && (row3[0] == 'X' || row3[0] == 'O');
-
-        char[] column1 = newBoard.getColumn(0);
-        char[] column2 = newBoard.getColumn(1);
-        char[] column3 = newBoard.getColumn(2);
-
-        boolean firstColumnVictory = (column1[0] == column1[1]) && (column1[1] == column1[2]) && (column1[0] == 'X' || column1[0] == 'O');
-        boolean secondColumnVictory = (column2[0] == column2[1]) && (column2[1] == column2[2]) && (column2[0] == 'X' || column2[0] == 'O');
-        boolean thirdColumnVictory = (column3[0] == column3[1]) && (column3[1] == column3[2]) && (column3[0] == 'X' || column3[0] == 'O');
-
-        char[] leftDiagonal = newBoard.getDiagonal('L');
-        char[] rightDiagonal = newBoard.getDiagonal('R');
-
-        boolean leftDiagonalVictory = (leftDiagonal[0] == leftDiagonal[1]) && (leftDiagonal[1] == leftDiagonal[2]) && (leftDiagonal[0] == 'X' || leftDiagonal[0] == 'O');
-        boolean rightDiagonalVictory = (rightDiagonal[0] == rightDiagonal[1]) && (rightDiagonal[1] == rightDiagonal[2]) && (rightDiagonal[0] == 'X' || rightDiagonal[0] == 'O');
-
-        System.out.println(firstRowVictory);
-        System.out.println(secondRowVictory);
-        System.out.println(thirdRowVictory);
-
-        System.out.println();
-
-        System.out.println(firstColumnVictory);
-        System.out.println(secondColumnVictory);
-        System.out.println(thirdColumnVictory);
-
-        System.out.println();
-
-        System.out.println(leftDiagonalVictory);
-        System.out.println(rightDiagonalVictory);
-
+        System.out.println(board.toString());
+        System.out.println(board.getWinner() + " wins!");
+        
+        input.close();
     }
     
+    private static int[] stringToInt(String str) {
+        try {
+            String[] split = str.split(",");
+            int[] move = {Integer.parseInt(split[0]), Integer.parseInt(split[1])};
+            return move;
+        } catch(Exception e) {
+            return null;
+        }
+    }
 }
